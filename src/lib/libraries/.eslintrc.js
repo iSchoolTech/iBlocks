6a040415089a5f45229210629efdbe294f93e6c3
Muhammad <21185758+Muhammad-Saad-01@users.noreply.github.com>
2023-04-16 23:35:55 +0200

    [skip ci] Build for $(git log --pretty=format:%H -n1)

--- src/lib/libraries/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    rules: {
-        // These manifest files use duplicate imports to make things easier to follow
-        // by providing clear parallel structure. Turn the error off for this folder.
-        'no-duplicate-imports': 0
-    }
-};
